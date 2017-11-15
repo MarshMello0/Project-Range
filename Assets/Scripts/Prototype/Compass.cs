7b5c12026e295c6abb18c524f83c2a989db49fbc
MarshMello <33008390+MarshMello0@users.noreply.github.com>
2017-11-16 00:37:58 +0100

    Clouds

---
+++ Assets/Scripts/Prototype/Compass.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+    [Header("Settings")]
+    public float compDeg;
+    public string compDir;
+    [Header("Objects")]
+    public Text compDegText;
+    public Text compDirText;
+    public Transform playerTra;
+    private void Update()
+    {
+        UpdateDegree();
+    }
+
+    void UpdateDegree()
+    {
+        compDeg = playerTra.localRotation.y;
+        compDegText.text = compDeg + "";
+    }
+
+}
